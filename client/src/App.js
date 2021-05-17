e0b9edf2691b08d5dee0885bc1837637c30104e2
iwood55 <isaac.ewood@icloud.com>
2021-05-17 18:42:44 -0400

    Finished project

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import Movie from './Movies/Movie.js';
+import {Route, Switch} from 'react-router-dom';
+import MovieList from './Movies/MovieList.js';
 import SavedList from './Movies/SavedList';
 
 export default function App () {
-  const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
+  // const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
 
   useEffect(() => {
@@ -14,6 +16,7 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          setMovieList(response.data)
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -22,15 +25,22 @@ export default function App () {
     getMovies();
   }, []);
 
-  const addToSavedList = id => {
-    // This is stretch. Prevent the same movie from being "saved" more than once
-  };
+  // const addToSavedList = id => {
+  //   // This is stretch. Prevent the same movie from being "saved" more than once
+  // };
 
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      <Switch>
+        <Route exact path = '/'>
+          <MovieList movies = {movieList}/>
+        </Route>
+        <Route path = '/movies/:id'>
+          <Movie movie = {movieList}/>
+        </Route>
+      </Switch>
     </div>
   );
 }
